79a7428b9821cb45b2c693c25ea75904f754a94b
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-05-09 15:36:49 +0530

    Update fakeinfo.py

--- DAXXMUSIC/plugins/Yumi/fakeinfo.py
+++ DAXXMUSIC/plugins/Yumi/fakeinfo.py
@@ -50,7 +50,7 @@ def generate_fake_user_by_country(client, message):
         street = user_info['location']['street']['name']
         zip_code = user_info['location']['postcode']
         # Reply with the generated fake user information for the specified country
-        message.reply_text(f"๏ ɴᴀᴍᴇ ➠ {first_name} {last_name}\n\n๏ ᴇᴍᴀɪʟ ➠ {email}\n\n๏ ᴄᴏᴜɴᴛʀʏ ➠ {country}\n\n๏ sᴛᴀᴛᴇ ➠ {state}\n\n๏ ᴄɪᴛʏ ➠ {city}\n\n๏ ᴀᴅᴅʀᴇss ➠ {street}\n\n๏ ᴢɪᴘ ᴄᴏᴅᴇ ➠ {zip_code}\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛ɴ ʏ ᴋ ᴀ ᴀ࿐ ")
+        message.reply_text(f"๏ ɴᴀᴍᴇ ➠ {first_name} {last_name}\n\n๏ ᴇᴍᴀɪʟ ➠ {email}\n\n๏ ᴄᴏᴜɴᴛʀʏ ➠ {country}\n\n๏ sᴛᴀᴛᴇ ➠ {state}\n\n๏ ᴄɪᴛʏ ➠ {city}\n\n๏ ᴀᴅᴅʀᴇss ➠ {street}\n\n๏ ᴢɪᴘ ᴄᴏᴅᴇ ➠ {zip_code}\n\n๏ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ ๛˹ɑᴍʀɪᴛ яᴀᴊ˼࿐ ")
     else:
         message.reply_text(f"✦ ғᴀɪʟᴇᴅ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ғᴀᴋᴇ ᴜsᴇʀ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ғᴏʀ {country_name}.")
 
