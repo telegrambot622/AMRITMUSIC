3cb09d87200f4510afce83015d606428cbeffcb2
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-05-09 16:00:42 +0530

    Update autoleave.py

--- DAXXMUSIC/plugins/misc/autoleave.py
+++ DAXXMUSIC/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001802990747
-                                and i.chat.id != -1001802990747
+                                and i.chat.id != -1002089329061
+                                and i.chat.id != -1002118567120
                             ):
                                 if left == 20:
                                     continue
